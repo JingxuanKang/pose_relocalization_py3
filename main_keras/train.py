a1c42127c257b42aff8871b6935a83f7ac0ed43e
Jingxuan Kang <jxkang01@gmail.com>
2022-07-11 11:15:30 +0100

    abc

--- main_keras/train.py
+++ main_keras/train.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
 import pickle
@@ -8,7 +9,6 @@ import pickle
 
 
 #force matplotlib use any Xwindows
-matplotlib.use('Agg')
 
 import sys
 cwd = os.getcwd()
